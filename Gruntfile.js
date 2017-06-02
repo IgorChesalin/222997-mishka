cb34e7ae5580dc18dea7cafec52dfac6dcc3c471
Igor Chesalin <IgorChesalin@gmail.com>
2017-06-02 16:03:11 +0300

    Добавил шрифты на индекс

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
     less: {
       style: {
         files: {
-          "css/style.css": "less/style.less"
+          "css/style.css": "less/*.less"
         }
       }
     },
